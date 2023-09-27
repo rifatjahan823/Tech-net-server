e97d737a628e2482356d8a2c93c5ca1e6009b236
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-27 20:17:05 +0600

    change url

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
 });
 
 const run = async () => {
