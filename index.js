709c3612ad49f29026a9ac6cca30c862f6697785
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-27 20:58:04 +0600

    change node

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eldyw2o.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eldyw2o.mongodb.net`
 
 const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
