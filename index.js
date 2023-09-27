a853aed4cb175f5a5e126c5b3a8098f4edb47b29
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-27 22:05:31 +0600

    again install mongodb

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eldyw2o.mongodb.net`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eldyw2o.mongodb.net/?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
